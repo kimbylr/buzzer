9728efa8a62f75779498ffbc809a6529ea60542f
jimynu <kim@bylr.ch>
2020-04-11 16:55:26 +0200

    make work on heroku

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-const title = 'Buffer Buzzer'
+const title = 'Zersch gsi!';
+const PORT = process.env.PORT || 3030;
 
 let data = {
   users: new Set(),
   buzzes: new Set(),
-}
+};
 
 const getData = () => ({
   users: [...data.users],
-  buzzes: [...data.buzzes].map(b => {
-    const [ name, team ] = b.split('-')
-    return { name, team }
-  })
-})
+  buzzes: [...data.buzzes].map((b) => {
+    const [name, team] = b.split('-');
+    return { name, team };
+  }),
+});
 
-app.use(express.static('public'))
-app.set('view engine', 'pug')
+app.use(cors()); // permits all requests
+app.use(express.static('public'));
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => res.render('index', { title }))
-app.get('/host', (req, res) => res.render('host', Object.assign({ title }, getData())))
+app.get('/', (req, res) => res.render('index', { title }));
+app.get('/host', (req, res) =>
+  res.render('host', Object.assign({ title }, getData())),
+);
 
 io.on('connection', (socket) => {
   socket.on('join', (user) => {
-    data.users.add(user.id)
-    io.emit('active', [...data.users].length)
-    console.log(`${user.name} joined!`)
-  })
+    data.users.add(user.id);
+    io.emit('active', [...data.users].length);
+    console.log(`${user.name} joined!`);
+  });
 
   socket.on('buzz', (user) => {
-    data.buzzes.add(`${user.name}-${user.team}`)
-    io.emit('buzzes', [...data.buzzes])
-    console.log(`${user.name} buzzed in!`)
-  })
+    data.buzzes.add(`${user.name}-${user.team}`);
+    io.emit('buzzes', [...data.buzzes]);
+    console.log(`${user.name} buzzed in!`);
+  });
 
   socket.on('clear', () => {
-    data.buzzes = new Set()
-    io.emit('buzzes', [...data.buzzes])
-    console.log(`Clear buzzes`)
-  })
-})
+    data.buzzes = new Set();
+    io.emit('buzzes', [...data.buzzes]);
+    console.log(`Clear buzzes`);
+  });
+});
 
-server.listen(8090, () => console.log('Listening on 8090'))
+server.listen(PORT, () => console.log('Listening on port', PORT));
